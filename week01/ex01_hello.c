4626480939664aa13255ce546910c36bfd045eb2
MrEggTart <khamkhon546@gmail.com>
2026-07-02 16:17:52 +0700

    Add flowcharts, pseudocode, and algorithms for grade checking and damage calculation
    - Created flowchart for determining the maximum of two numbers and displaying grades based on score.
    - Added pseudocode for grade evaluation and counting from 1 to N.
    - Implemented algorithms for calculating damage in a game scenario and handling experience points for leveling up.
    - Established a new directory for flowchart files.

---
+++ week01/ex01_hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()  
+{
+    printf("สวัสดีครับ!, ผม นายรณรณก กองกมล\n");
+    printf("รหัสนักศึกษา:692110162\n");
+    printf("รหัสวิชา: DG111 Computer Programming");
+    return 0;
+}
